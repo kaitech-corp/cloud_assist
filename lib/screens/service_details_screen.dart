fd0bd0c2f0caf0c6eb253004498e2698f9c22f31
randynolden <randy@kaitechcorp.com>
2023-01-20 11:14:10 -0800

    feat: layout update

--- lib/screens/service_details_screen.dart
+++ lib/screens/service_details_screen.dart
@@ -4,6 +4,7 @@ import 'package:readmore/readmore.dart';
 import '../models/cloud_data_model.dart';
 import '../services/constants.dart';
 import '../services/functions.dart';
+import '../services/navigation/navigation.dart';
 import '../services/service_config/service_config.dart';
 import '../services/text_styles.dart';
 import '../widgets/service_detail_card.dart';
@@ -25,118 +26,144 @@ class _ServiceDetailsState extends State<ServiceDetails> {
     ];
     return Scaffold(
         body: SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: SizedBox(
-            height: SizeConfig.screenHeight,
-            width: SizeConfig.screenWidth,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 8.0),
-                  child: Text(
-                    widget.serviceData.service,
-                    style: headline4(context),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 16.0),
-                  child: Text(widget.serviceData.description,
-                      style: subtitle1(context)),
-                ),
-                SizedBox(height: SizeConfig.screenHeight * .05),
-                Text('Full Description', style: headline6(context)),
-                Padding(
-                  padding: const EdgeInsets.only(top: 16.0),
-                  child: ReadMoreText(
-                    widget.serviceData.detail,
-                    trimMode: TrimMode.Line,
-                    trimLines: 2,
-                    trimCollapsedText: 'More',
-                    trimExpandedText: 'Less',
-                    style: subtitle1(context),
-                    colorClickableText: Colors.pinkAccent,
-                  ),
-                ),
-                SizedBox(height: SizeConfig.screenHeight * .05),
-                SizedBox(
-                  height: SizeConfig.screenHeight * .1,
-                  width: SizeConfig.screenWidth,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: List<Widget>.generate(
-                          serviceDetailItems.length,
-                          (int index) => Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Column(
-                                  children: <Widget>[
-                                    CircleAvatar(
-                                      child: IconButton(
-                                          onPressed: () {
-                                            showModalBottomSheet(
-                                                context: context,
-                                                builder: (BuildContext
-                                                        context) =>
-                                                    ServiceDetailsBottomSheet(
-                                                        data: serviceDataList[
-                                                            index]),
-                                                shape: RoundedRectangleBorder(
-                                                    borderRadius:
-                                                        BorderRadius.circular(
-                                                            20)));
-                                          },
-                                          icon: serviceDetailIcons[index]),
-                                    ),
-                                    Text(serviceDetailItems[index])
-                                  ],
-                                ),
-                              ))),
-                ),
-                SizedBox(
-                    height: SizeConfig.screenHeight * .15,
-                    width: SizeConfig.screenWidth,
-                    child: funFactCard(const Text('Fun Facts'))),
-                Expanded(
-                  child: Align(
-                      alignment: Alignment.bottomCenter,
-                      child: ElevatedButton(
-                          onPressed: () {
-                            launchUrlFunc(widget.serviceData.link);
-                          },
-                          child: Text(
-                            'Learn More',
-                            style: subtitle1(context),
-                          ))),
-                )
-              ],
-            )),
-      ),
-    ));
-  }
-}
+      child: SizedBox(
+          height: SizeConfig.screenHeight,
+          width: SizeConfig.screenWidth,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        router.pop();
+                      },
+                      icon: const Icon(Icons.arrow_back_ios))
+                ],
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 8.0),
+                        child: Text(
+                          widget.serviceData.service,
+                          style: headline4(context),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 16.0),
+                        child: Text(widget.serviceData.description,
+                            style: subtitle1(context)),
+                      ),
+                      SizedBox(height: SizeConfig.screenHeight * .05),
+                      Text('Full Description', style: headline6(context)),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 16.0),
+                        child: ReadMoreText(
+                          widget.serviceData.detail,
+                          trimMode: TrimMode.Line,
+                          trimLines: 2,
+                          trimCollapsedText: 'More',
+                          trimExpandedText: 'Less',
+                          style: subtitle1(context),
+                          colorClickableText: Colors.pinkAccent,
+                        ),
+                      ),
+                      SizedBox(height: SizeConfig.screenHeight * .05),
+                      SizedBox(
+                        height: SizeConfig.screenHeight * .1,
+                        width: SizeConfig.screenWidth,
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: List<Widget>.generate(
+                                serviceDetailItems.length,
+                                (int index) => Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Column(
+                                        children: <Widget>[
+                                          CircleAvatar(
+                                            child: IconButton(
+                                                onPressed: () {
+                                                  showModalBottomSheet(
+                                                      context: context,
+                                                      builder: (BuildContext
+                                                              context) =>
+                                                          ServiceDetailsBottomSheet(
+                                                              index: index,
+                                                              data:
+                                                                  serviceDataList[
+                                                                      index]),
+                                                      shape:
+                                                          RoundedRectangleBorder(
+                                                              borderRadius:
+                                                                  BorderRadius
+                                                                      .circular(
+                                                                          20)));
+                                                },
+                                                icon:
+                                                    serviceDetailIcons[index]),
+                                          ),
+                                          Text(serviceDetailItems[index])
+                                        ],
+                                      ),
+                                    ))),
+                      ),
+                      SizedBox(
+                          height: SizeConfig.screenHeight * .15,
+                          width: SizeConfig.screenWidth,
+                          child: funFactCard(const Text('Fun Facts'))),
+                      Expanded(
+                        child: Align(
+                            alignment: Alignment.bottomCenter,
+                            child: ElevatedButton(
+                                onPressed: () {
+                                  launchUrlFunc(widget.serviceData.link);
+                                },
+                                child: Text(
+                                  'Learn More',
+                                  style: subtitle1(context),
+                                ))),   ), ],  ), ),  ) ],  )),));}}
 
 class ServiceDetailsBottomSheet extends StatelessWidget {
-  const ServiceDetailsBottomSheet({super.key, required this.data});
+  const ServiceDetailsBottomSheet(
+      {super.key, required this.data, required this.index});
   final List<String> data;
+  final int index;
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: List<Widget>.generate(
-            data.length,
-            (int index) => Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: card(Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      data[index],
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                  )),
-                )),
-      ),
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            serviceDetailItems[index],
+            style: headline4(context),
+          ),
+        ),
+        Flexible(
+          child: SingleChildScrollView(
+            child: Column(
+              children: List<Widget>.generate(
+                  data.length,
+                  (int index) => Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: card(Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            data[index],
+                            style: Theme.of(context).textTheme.headline6,
+                          ),
+                        )),
+                      )),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
