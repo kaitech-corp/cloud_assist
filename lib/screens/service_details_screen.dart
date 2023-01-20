df9f2be2cd00b4c3ab369c5698b01a32d2c4d0b1
randynolden <randy@kaitechcorp.com>
2023-01-20 10:35:52 -0800

    feat: new details screen

---
+++ lib/screens/service_details_screen.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:readmore/readmore.dart';
+
+import '../models/cloud_data_model.dart';
+import '../services/constants.dart';
+import '../services/functions.dart';
+import '../services/service_config/service_config.dart';
+import '../services/text_styles.dart';
+import '../widgets/service_detail_card.dart';
+
+class ServiceDetails extends StatefulWidget {
+  const ServiceDetails({super.key, required this.serviceData});
+  final CloudData serviceData;
+  @override
+  State<ServiceDetails> createState() => _ServiceDetailsState();
+}
+
+class _ServiceDetailsState extends State<ServiceDetails> {
+  @override
+  Widget build(BuildContext context) {
+    final List<List<String>> serviceDataList = <List<String>>[
+      widget.serviceData.benefits,
+      widget.serviceData.cons,
+      widget.serviceData.useCases
+    ];
+    return Scaffold(
+        body: SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: SizedBox(
+            height: SizeConfig.screenHeight,
+            width: SizeConfig.screenWidth,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                    widget.serviceData.service,
+                    style: headline4(context),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16.0),
+                  child: Text(widget.serviceData.description,
+                      style: subtitle1(context)),
+                ),
+                SizedBox(height: SizeConfig.screenHeight * .05),
+                Text('Full Description', style: headline6(context)),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16.0),
+                  child: ReadMoreText(
+                    widget.serviceData.detail,
+                    trimMode: TrimMode.Line,
+                    trimLines: 2,
+                    trimCollapsedText: 'More',
+                    trimExpandedText: 'Less',
+                    style: subtitle1(context),
+                    colorClickableText: Colors.pinkAccent,
+                  ),
+                ),
+                SizedBox(height: SizeConfig.screenHeight * .05),
+                SizedBox(
+                  height: SizeConfig.screenHeight * .1,
+                  width: SizeConfig.screenWidth,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: List<Widget>.generate(
+                          serviceDetailItems.length,
+                          (int index) => Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  children: <Widget>[
+                                    CircleAvatar(
+                                      child: IconButton(
+                                          onPressed: () {
+                                            showModalBottomSheet(
+                                                context: context,
+                                                builder: (BuildContext
+                                                        context) =>
+                                                    ServiceDetailsBottomSheet(
+                                                        data: serviceDataList[
+                                                            index]),
+                                                shape: RoundedRectangleBorder(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            20)));
+                                          },
+                                          icon: serviceDetailIcons[index]),
+                                    ),
+                                    Text(serviceDetailItems[index])
+                                  ],
+                                ),
+                              ))),
+                ),
+                SizedBox(
+                    height: SizeConfig.screenHeight * .15,
+                    width: SizeConfig.screenWidth,
+                    child: funFactCard(const Text('Fun Facts'))),
+                Expanded(
+                  child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: ElevatedButton(
+                          onPressed: () {
+                            launchUrlFunc(widget.serviceData.link);
+                          },
+                          child: Text(
+                            'Learn More',
+                            style: subtitle1(context),
+                          ))),
+                )
+              ],
+            )),
+      ),
+    ));
+  }
+}
+
+class ServiceDetailsBottomSheet extends StatelessWidget {
+  const ServiceDetailsBottomSheet({super.key, required this.data});
+  final List<String> data;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: List<Widget>.generate(
+            data.length,
+            (int index) => Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: card(Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      data[index],
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                  )),
+                )),
+      ),
+    );
+  }
+}
