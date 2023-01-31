4e474e05f0232df640d9d03f5d959261ae392cd4
randynolden <randy@kaitechcorp.com>
2023-01-31 11:47:50 -0800

    feat:ui update

--- lib/widgets/service_detail_card.dart
+++ lib/widgets/service_detail_card.dart
@@ -4,23 +4,27 @@ import 'package:styled_widget/styled_widget.dart';
 Widget serviceDetailCard(Widget child) => Styled.widget(child: child)
     .padding(all: 10)
     .decorated(
-  color: Colors.white54,
-  borderRadius: BorderRadius.circular(10),
-)
+      color: Colors.white54,
+      borderRadius: BorderRadius.circular(10),
+    )
     .elevation(10);
 
 Widget card(Widget child) => Styled.widget(child: child)
     .padding(all: 30)
     .decorated(
-  color: Colors.white70,
-  borderRadius: BorderRadius.circular(10),
-)
+      color: Colors.white70,
+      borderRadius: BorderRadius.circular(10),
+    )
     .elevation(10);
 
 Widget funFactCard(Widget child) => Styled.widget(child: child)
     .padding(all: 30)
     .decorated(
-  color: Colors.amber,
-  borderRadius: BorderRadius.circular(25),
-)
-    .elevation(15);
+      color: Colors.amber,
+      borderRadius: const BorderRadius.all(Radius.circular(30)),
+    )
+    .card(
+        elevation: 10,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ));
