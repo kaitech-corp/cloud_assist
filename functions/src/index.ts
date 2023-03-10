cb7ccc479f5d960e3bbefcd368f052bbfb63f861
randynolden <randy@kaitechcorp.com>
2023-03-10 11:11:37 -0800

    feat:cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,11 @@
-import {https} from "firebase-functions";
+import {SecretManagerServiceClient} from "@google-cloud/secret-manager";
+import * as functions from "firebase-functions";
 import axios from "axios";
 import {initializeApp} from "firebase-admin/app";
 initializeApp();
 
 //  Combined data api call.
-exports.getCombinedData = https.onCall(async (data, context) => {
+exports.getCombinedData = functions.https.onCall(async (data, context) => {
   const url =
     "https://storage.googleapis.com/api-project-371618.appspot.com/combined_data.json";
 
@@ -22,7 +23,7 @@ exports.getCombinedData = https.onCall(async (data, context) => {
 });
 
 //  Cloud data api call.
-exports.getCloudData = https.onCall(async (data, context) => {
+exports.getCloudData = functions.https.onCall(async (data, context) => {
   const url =
     "https://storage.googleapis.com/api-project-371618.appspot.com/cloud_data.json";
 
@@ -40,7 +41,7 @@ exports.getCloudData = https.onCall(async (data, context) => {
 });
 
 //    Networking data api call.
-exports.getNetworkingData = https.onCall(async (data, context) => {
+exports.getNetworkingData = functions.https.onCall(async (data, context) => {
   const url =
     "https://storage.googleapis.com/api-project-371618.appspot.com/cloud_networking.json";
 
@@ -58,7 +59,7 @@ exports.getNetworkingData = https.onCall(async (data, context) => {
 });
 
 //    Database data api call.
-exports.getDatabaseData = https.onCall(async (data, context) => {
+exports.getDatabaseData = functions.https.onCall(async (data, context) => {
   const url =
     "https://storage.googleapis.com/api-project-371618.appspot.com/database_data.json";
 
@@ -76,7 +77,7 @@ exports.getDatabaseData = https.onCall(async (data, context) => {
 });
 
 //    Security data api call.
-exports.getSecurityData = https.onCall(async (data, context) => {
+exports.getSecurityData = functions.https.onCall(async (data, context) => {
   const url =
     "https://storage.googleapis.com/api-project-371618.appspot.com/security_data.json";
 
@@ -94,7 +95,7 @@ exports.getSecurityData = https.onCall(async (data, context) => {
 });
 
 //    Gcloud command line data api call.
-exports.getGCloudData = https.onCall(async (data, context) => {
+exports.getGCloudData = functions.https.onCall(async (data, context) => {
   const url =
     "https://storage.googleapis.com/api-project-371618.appspot.com/cmd_line_cheat_sheet.json";
 
@@ -112,7 +113,7 @@ exports.getGCloudData = https.onCall(async (data, context) => {
 });
 
 //    Facts api call.
-exports.getFactsData = https.onCall(async (data, context) => {
+exports.getFactsData = functions.https.onCall(async (data, context) => {
   const url =
     "https://storage.googleapis.com/api-project-371618.appspot.com/facts.json";
 
@@ -126,5 +127,55 @@ exports.getFactsData = https.onCall(async (data, context) => {
   } catch (error) {
     console.error(`Error in facts data API call: ${error}`);
     return "Error in facts data API call";
+  }
+});
+
+//  Create new fun facts
+exports.createFacts = functions.https.onCall(async (snap, context) => {
+  const service = snap.service;
+  const prompt =
+    "Tell me 3 interesting facts\n" +
+    "about Google Cloud's " +
+    service +
+    "}\n" +
+    "And here's an example output in a JSON array format:\n" +
+    "[{'fun_fact': 'Did you know that [fact 1 about the GCP service]?'},\n" +
+    "{'fun_fact': 'Companies like [fact 2 about the GCP service].'},\n" +
+    "{'fun_fact': 'A little known fact about'" +
+    "[fact 3 about the GCP service]}]";
+  const tokenSize = 2000;
+  const temp = 0.7;
+  const uri = "https://api.openai.com/v1/completions";
+  const data = {
+    "model": "text-davinci-003",
+    "prompt": prompt,
+    "max_tokens": tokenSize,
+    "temperature": temp,
+  };
+  try {
+    // Set up Secret Manager client
+    const projectId = "247644706560";
+    const client = new SecretManagerServiceClient();
+
+    // Get the secret
+    const name = "openAI_api_key";
+    const [version] = await client.accessSecretVersion({
+      name: `projects/${projectId}/secrets/${name}/versions/latest`,
+    });
+    const secretValue = version.payload.data.toString();
+
+    const config = {
+      headers: {
+        "Authorization": `Bearer ${secretValue}`,
+        "Content-Type": "application/json",
+      },
+    };
+    // Do something with the secret value
+    console.log(`My secret is ${secretValue}`);
+    const response = await axios.post(uri, data, config);
+    const result = response.data;
+    console.log(result);
+  } catch (err) {
+    console.error(err);
   }
 });
